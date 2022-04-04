cc2c9ffc6210f821ecf99a19f4a276f4ec1f35d5
SangKwon <sangkwon@innofin.co.kr>
2022-04-04 15:13:37 +0900

    fix: 랜딩페이지2를 수정하다

--- src/components/landing/Landing2.tsx
+++ src/components/landing/Landing2.tsx
@@ -2,19 +2,42 @@
 import { Helmet, HelmetProvider } from "react-helmet-async";
 import ReactPixel from "react-facebook-pixel";
 import "./style/style.css";
-const advancedMatching = { em: "support@innofin.co.kr" };
 const options = {
   autoConfig: true,
   debug: false,
 };
 ReactPixel.track("PageView");
 ReactPixel.track("ViewContent");
-ReactPixel.init("1135810613546802", advancedMatching, options);
+ReactPixel.init("3172269179709072");
 
 const Landing2 = () => {
   const handleClick = () => {
     ReactPixel.track("ViewContent");
   };
+
+  const initGA = () => {
+    window.dataLayer = window.dataLayer || [];
+    function gtag() {
+      dataLayer.push(arguments);
+    }
+    gtag("js", new Date());
+    gtag("config", "AW-10869743597");
+  };
+  initGA();
+
+  function gtag_report_conversion(url) {
+    var callback = function () {
+      if (typeof url != "undefined") {
+        window.location = url;
+      }
+    };
+    gtag("event", "conversion", {
+      send_to: "AW-10869743597/uQnaCN2epKsDEO2_jL8o",
+      event_callback: callback,
+    });
+    return false;
+  }
+
   return (
     <>
       <HelmetProvider>
@@ -30,56 +53,98 @@ const Landing2 = () => {
             content="wkuc1g0dofg634y6nz0t2z3kc9vr34"
           />
           <title>후달리던 투자가 투달해서 올랐다</title>
-
-          <script></script>
-          {/* <noscript>
-            <img
-              height="1"
-              width="1"
-              style={{ display: "none" }}
-              src="https://www.facebook.com/tr?id=1135810613546802&ev=PageView&noscript=1"
-              alt=""
-            />
-          </noscript> */}
+          <script
+            async
+            src="https://www.googletagmanager.com/gtag/js?id=AW-10869743597"
+          ></script>
         </Helmet>
       </HelmetProvider>
       <div id="container">
         <div class="topWrap">
-          <div class="top">
-            <img src="../img/topcont.png" alt="" />
-            <div class="btn_wrap">
-              <a
-                href="https://play.google.com/store/apps/details?id=com.tudal.tp"
-                id="button_link"
-                onClick={handleClick}
-              >
-                <img src="../img/btn_g.svg" alt="" />
-              </a>
-              <a
-                href="https://apps.apple.com/kr/app/id1455175051"
-                id="button_link"
-                onClick={handleClick}
-              >
-                <img src="../img/btn_a.svg" alt="" />
-              </a>
+          <div class="topbg">
+            <div class="top contImg">
+              <img src="../img/topcont.png" alt="" class="contImg pc_view" />
+              <img src="../img/topcont_mo.png" alt="" class="contImg mo_view" />
+              <div class="btn_wrap">
+                <a
+                  href="https://play.google.com/store/apps/details?id=com.tudal.tp"
+                  id="button_link"
+                  onClick={() =>
+                    gtag_report_conversion(
+                      "https://play.google.com/store/apps/details?id=com.tudal.tp"
+                    )
+                  }
+                >
+                  <img src="img/btn_g.svg" alt="" />
+                </a>
+                <a
+                  href="https://apps.apple.com/kr/app/id1455175051"
+                  id="button_link"
+                  onClick={() =>
+                    gtag_report_conversion(
+                      "https://apps.apple.com/kr/app/id1455175051"
+                    )
+                  }
+                >
+                  <img src="img/btn_a.svg" alt="" />
+                </a>
+              </div>
             </div>
           </div>
         </div>
 
         <div class="cont_01 content">
-          <img src="../img/content_01.png" alt="" />
+          <img src="../img/content_01.png" alt="" class="contImg pc_view" />
+          <img src="../img/content_01_mo.png" alt="" class="contImg mo_view" />
         </div>
 
         <div class="cont_02 content">
-          <img src="../img/content_02.png" alt="" />
+          <img src="../img/content_02.png" alt="" class="contImg pc_view" />
+          <img src="..img/content_02_mo.png" alt="" class="contImg mo_view" />
         </div>
 
         <div class="cont_03 content">
-          <img src="../img/content_03.png" alt="" />
+          <img src="../img/content_03_01.png" alt="" class="contImg pc_view" />
+          <img
+            src="../img/content_03_01_mo.png"
+            alt=""
+            class="contImg mo_view"
+          />
+          <div class="btn_wrap contentBtn contImg">
+            <a
+              href="https://play.google.com/store/apps/details?id=com.tudal.tp"
+              id="button_link"
+              onClick={() =>
+                gtag_report_conversion(
+                  "https://play.google.com/store/apps/details?id=com.tudal.tp"
+                )
+              }
+            >
+              <img src="img/btn_g.svg" alt="" />
+            </a>
+            <a
+              href="https://apps.apple.com/kr/app/id1455175051"
+              id="button_link"
+              onClick={() =>
+                gtag_report_conversion(
+                  "https://apps.apple.com/kr/app/id1455175051"
+                )
+              }
+            >
+              <img src="img/btn_a.svg" alt="" />
+            </a>
+          </div>
+          <img src="../img/content_03_02.png" alt="" class="contImg pc_view" />
+          <img
+            src="../img/content_03_02_mo.png"
+            alt=""
+            class="contImg mo_view"
+          />
         </div>
 
         <div class="bottom">
-          <img src="../img/bottom_txt.png" alt="" />
+          <img src="../img/bottom_txt.png" alt="" class="contImg pc_view" />
+          <img src="../img/bottom_txt_mo.png" alt="" class="contImg mo_view" />
         </div>
       </div>
     </>
